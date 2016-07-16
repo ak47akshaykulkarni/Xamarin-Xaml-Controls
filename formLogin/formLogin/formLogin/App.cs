58913121f330016aacd006b58e2a0be7b4bfe086
akshayk <akshay@quexst.com>
2016-07-16 17:56:52 +0530

    Xaml Controls

---
+++ formLogin/formLogin/formLogin/App.cs
@@ -0,0 +1,81 @@
+﻿using System.Threading.Tasks;
+using Org.Json;
+using System.Net;
+using System.IO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Plugin.DeviceInfo;
+
+using Xamarin.Forms;
+
+namespace formLogin
+{
+    public class App : Application
+    {
+        public App()
+        {
+            MainPage =new NavigationPage( new Page2());
+
+
+            //MainPage = new ContentPage
+            //{
+            //    Content = new ScrollView
+            //    {
+            //        Content = new StackLayout
+            //    {
+
+            //        Padding = 50,
+            //        VerticalOptions = LayoutOptions.Center,
+            //        Children = {
+            //             new Label{ Text = "Generated AppId: " + CrossDeviceInfo.Current.GenerateAppId()},
+            //  new Label{ Text = "Generated AppId: " + CrossDeviceInfo.Current.GenerateAppId(true)},
+            //  new Label{ Text = "Generated AppId: " + CrossDeviceInfo.Current.GenerateAppId(true, "hello")},
+            //  new Label{ Text = "Generated AppId: " + CrossDeviceInfo.Current.GenerateAppId(true, "hello", "world")},
+            //  new Label{ Text = "Id: " + CrossDeviceInfo.Current.Id},
+            //  new Label{ Text = "Model: " + CrossDeviceInfo.Current.Model},
+            //  new Label{ Text = "Platform: " + CrossDeviceInfo.Current.Platform},
+            //  new Label{ Text = "Version: " + CrossDeviceInfo.Current.Version},
+            //        }
+            //    }
+            //}
+            //};
+
+
+            // The root page of your application
+            //MainPage = new ContentPage
+            //{
+            //    Content = new ScrollView
+            //    {
+            //        Content = new StackLayout
+            //        {
+            //            Padding = 50,
+            //            VerticalOptions = LayoutOptions.Start,
+            //            Children = {
+            //            mEmail,
+            //            mPassword,
+            //            btnLogin,
+            //            listedhere
+            //        }
+            //        }
+            //    }
+            //};
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ formLogin/formLogin/formLogin/Page2.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Plugin.Connectivity;
+using Plugin.DeviceInfo;
+
+using Xamarin.Forms;
+
+namespace formLogin
+{
+    public partial class Page2 : ContentPage
+    {
+        public Page2()
+        {
+            InitializeComponent();
+            Title = "XAML Views Inside Scroll View";
+            bool sad = Plugin.Connectivity.CrossConnectivity.Current.IsConnected;
+            var platform = Device.OS.ToString();
+            Title = platform;
+        }
+    }
+}
